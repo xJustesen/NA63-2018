b37c9e19e912a63bfe63ce6a149ebff2d1aece8c
xjustesen <jensbojustesen@gmail.com>
2018-08-09 16:07:33 +0200

    Removed uncessary declerations, added normal distro

--- src/globalvars.h
+++ src/globalvars.h
@@ -2,11 +2,7 @@
 #define GLOBALVARS_H
 
 default_random_engine generator;
-normal_distribution<double> xdistro;
-normal_distribution<double> ydistro;
-uniform_int_distribution<int> hits_per_event;
-normal_distribution<double> x_entry_angles;
-normal_distribution<double> y_entry_angles;
+normal_distribution<double> R_normal;
 uniform_real_distribution<double> R;
 
 #endif
