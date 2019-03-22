072dbf3f733b4548a31e984bf055e298dfc0f46b
xjustesen <jensbojustesen@gmail.com>
2019-03-22 13:50:52 +0100

    Moved main header files to seperate file

---
+++ src/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "./lib/analyser.h"
+#include "./lib/simulator.h"
+#include "./lib/auxillary_functions.h"
+#include "./lib/pre_processor.h"
+#include "./lib/run_simulation.h"
+
+#endif
